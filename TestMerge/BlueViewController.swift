78f26eb83a9e370365b15559eae57511f1878439
Enrique Sanchez <enrike.sanchez@tec.mx>
2021-09-13 18:10:28 -0700

    TEST-1234 My First Blue Controller Changes

---
+++ TestMerge/BlueViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BlueViewController.swift
+//  TestMerge
+//
+//  Created by user193555 on 9/13/21.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
