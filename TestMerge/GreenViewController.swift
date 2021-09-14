59ccf6b24be186054c4e0f629a47fef582e678c8
Enrique Sanchez <enrike.sanchez@tec.mx>
2021-09-13 18:24:44 -0700

    TEST-4567 Added changes for Green View Controller

---
+++ TestMerge/GreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GreenViewController.swift
+//  TestMerge
+//
+//  Created by user193555 on 9/13/21.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
